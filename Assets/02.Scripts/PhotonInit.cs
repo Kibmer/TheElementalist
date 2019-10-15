f4400fa988163667ec0f61a5eb0ed0ce140eb645
Kibmer <kkb9243@naver.com>
2019-10-15 10:10:11 +0900

    photon test

---
+++ Assets/02.Scripts/PhotonInit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PhotonInit : MonoBehaviour
+{
+    private string gameVersion = "1.0";
+    public string userId = "Kibmer";
+    public byte maxPlayer = 20;
+
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    private void Start()
+    {
+        PhotonNetwork.GameVersion = this.gameVersion;
+        PhotonNetwork.NickName = userId;
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    void Update()
+    {
+        
+    }
+}
