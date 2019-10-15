2b413f6526f0cb6e0181127964648236021d8010
veroxxx <54518869+veroxxx@users.noreply.github.com>
2019-10-15 21:58:52 +0900

    20191015

---
+++ Assets/02.Scripts/CubeExplosion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeExplosion : MonoBehaviour
+{
+    //public GameObject p_debris;                //파편큐브프리팹
+    public float debrisF;                        //파편프리팹 날아갈 세기
+    public Rigidbody[] debRb;                    //파편프리팹 Rb 
+
+    public void Start()
+    {
+
+    }
+    public void Explosion()
+    {
+        transform.Find("Debris").gameObject.SetActive(true);
+        //Debug.Log(transform.Find("Debris").gameObject.name);
+        transform.Find("LOD").gameObject.SetActive(false);
+        //Debug.Log(transform.Find("LOD").gameObject.name);
+        for (int i = 0; i < debRb.Length; i++)
+        {
+            debRb[i].AddExplosionForce(debrisF, transform.position, 1.0f);
+        }
+
+    }
+}
---
+++ Assets/02.Scripts/MouseCtrl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCtrl : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, 100.0f))
+            {
+                hit.transform.GetComponent<CubeExplosion>().Explosion();
+            }
+        }
+    }
+}
