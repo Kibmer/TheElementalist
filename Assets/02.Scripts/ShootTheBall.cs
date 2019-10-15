d0c4d6d886172aec56b30e1e0d3340fbca4094be
Kibmer <kkb9243@naver.com>
2019-10-15 11:19:45 +0900

    Photon test

--- Assets/02.Scripts/ShootTheBall.cs
+++ Assets/02.Scripts/ShootTheBall.cs
@@ -1,14 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class ShootTheBall : MonoBehaviour
+public class ShootTheBall : MonoBehaviourPunCallbacks
 {
     private Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if(!photonView.IsMine) return;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            photonView.RPC("Fire", RpcTarget.AllViaServer, null);
+        }
+    }
+    [PunRPC]
+    private void Fire()
+    {
         rb.AddForce(0, 500, 2000);
     }
 }
