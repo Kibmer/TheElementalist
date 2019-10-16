0966df2fed70ab3cd7d25b046e24924b54c98f97
Kibmer <kkb9243@naver.com>
2019-10-16 13:49:23 +0900

    Cannon instantiate complete

---
+++ Assets/02.Scripts/PlayerInitializer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerInitializer : MonoBehaviourPunCallbacks
+{
+    public Transform spawnPoint1;
+    public Transform spawnPoint2;
+
+    private void Awake()
+    {
+        Debug.Log(PhotonNetwork.CountOfPlayersInRooms);
+        if (PhotonNetwork.CountOfPlayersInRooms >= 1)
+        {
+            transform.position = spawnPoint2.position;
+            transform.Rotate(0, 180, 0);
+        }
+        else
+        {
+            transform.position = spawnPoint1.position;
+        }
+    }
+
+    private void Start()
+    {
+        Vector3 cannonSpawnPos = new Vector3(0, 0, Vector3.forward.z * 1.5f);
+        PhotonNetwork.Instantiate("Cannon"
+                    , transform.position + cannonSpawnPos
+                    , transform.rotation
+                    , 0);
+    }
+}
