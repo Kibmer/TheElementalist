00b0548ae6c216c7bf4f1d82d2bc6819e90df94d
Kibmer <kkb9243@naver.com>
2019-10-16 23:25:06 +0900

    Add turn flame system, sync prefab

--- Assets/02.Scripts/MGManager.cs
+++ Assets/02.Scripts/MGManager.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 
-public class PlayerInitializer : MonoBehaviourPunCallbacks
+public class MGManager : MonoBehaviourPunCallbacks
 {
+    public static MGManager instance = null;
+
     public Transform spawnPoint1;
     public Transform spawnPoint2;
 
+    public bool myTurn = false;
+
+    public GameObject[] turnFlames;
+
     private void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
         Debug.Log(PhotonNetwork.CountOfPlayersInRooms);
         if (PhotonNetwork.CountOfPlayersInRooms >= 1)
         {
@@ -29,10 +44,34 @@ public class PlayerInitializer : MonoBehaviourPunCallbacks
 
     private void Start()
     {
+        SetTurnFlame();
         Vector3 cannonSpawnPos = new Vector3(0, 0, Vector3.forward.z * 1.5f);
         PhotonNetwork.Instantiate("Cannon"
                     , transform.position + cannonSpawnPos
                     , transform.rotation
                     , 0);
+    }
+
+    public void EndTurn()
+    {
+        myTurn = !myTurn;
+    }
+
+    public void SetTurnFlame()
+    {
+        if (myTurn)
+        {
+            foreach (var flame in turnFlames)
+            {
+                flame.SetActive(true);
+            }
+        }
+        else
+        {
+            foreach (var flame in turnFlames)
+            {
+                flame.SetActive(false);
+            }
+        }
     }
 }
