7c67362de8587c67f85fa8317d2307bbfd0a9f21
weed6 <pjs1567@gmail.com>
2019-10-16 14:06:32 +0900

    element_ui

--- Assets/02.Scripts/Player/LeftHandController.cs
+++ Assets/02.Scripts/Player/LeftHandController.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
 
-public class LeftHandController : MonoBehaviour
+public class RightHandController : MonoBehaviour
 {
     public SteamVR_Input_Sources hand = SteamVR_Input_Sources.Any;
     public SteamVR_Input_Sources lefthand = SteamVR_Input_Sources.LeftHand;
     public SteamVR_Input_Sources righthand = SteamVR_Input_Sources.RightHand;
+    public SteamVR_Action_Boolean grab = SteamVR_Actions.default_GrabGrip;
     public SteamVR_Action_Boolean ControllerTrigger = SteamVR_Actions.default_InteractUI;
     public SteamVR_Behaviour_Pose pose;
     public SteamVR_Action_Vibration haptic = SteamVR_Actions.default_Haptic;
 
-    
 
     void Start()
     {
@@ -21,6 +21,8 @@ public class LeftHandController : MonoBehaviour
 
     void Update()
     {
-        
+
     }
+
+
 }
---
+++ Assets/02.Scripts/Player/RightHandController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class LeftHandController : MonoBehaviour
+{
+    public SteamVR_Input_Sources hand = SteamVR_Input_Sources.Any;
+    public SteamVR_Input_Sources lefthand = SteamVR_Input_Sources.LeftHand;
+    public SteamVR_Input_Sources righthand = SteamVR_Input_Sources.RightHand;
+    public SteamVR_Action_Boolean grab = SteamVR_Actions.default_GrabGrip;
+    //public SteamVR_Action_Boolean ControllerTrigger = SteamVR_Actions.default_InteractUI;
+    //public SteamVR_Behaviour_Pose pose;
+    //public SteamVR_Action_Vibration haptic = SteamVR_Actions.default_Haptic;
+
+    public GameObject ui_element;
+
+    void Start()
+    {
+        ui_element = transform.GetChild(1).gameObject;
+        Debug.Log(ui_element.name);
+    }
+
+    void Update()
+    {
+        if(grab.GetState(righthand))
+        {
+            Debug.Log("@@@@@@@@@");
+            ui_element.SetActive(true);
+        }
+
+        ui_element.SetActive(false);
+    }
+
+    
+
+}
